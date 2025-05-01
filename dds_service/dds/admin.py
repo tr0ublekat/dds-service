fae11de3dccd2346a78923a7818b24e23687d3fe
tr0ublekat <kirillkojev8@gmail.com>
2025-05-01 13:25:04 +0300

    Добавил в админку модели

--- dds_service/dds/admin.py
+++ dds_service/dds/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Status, Type, Category, Subcategory, DDSRecord
 
-# Register your models here.
+
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type')
+    list_filter = ('type',)
+
+
+@admin.register(Subcategory)
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+    list_filter = ('category',)
+
+
+@admin.register(DDSRecord)
+class DDSRecordAdmin(admin.ModelAdmin):
+    list_display = ('date', 'status', 'type', 'category', 'subcategory', 'amount')
+    list_filter = ('status', 'type', 'category', 'subcategory')
+    search_fields = ('comment',)
